e68b6c3842de379f8914996a37001864a29fc8f4
Yury <yurydgfuentes@gmail.com>
2023-03-19 01:10:52 +0200

    marketing: add components and little boilerplate

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+// Mount Function
+const mount = (el) => {
+  ReactDOM.render(<App />, el)
+}
+
+// In dev mode and in isolation call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root')
+  if (devRoot) {
+    mount(devRoot)
+  }
+}
+
+// Otherwise we will assume we are running through container
+// and we should export the mount function
